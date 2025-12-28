b54d9aebef30a3ce787f1a9ec6fc53a724e163f7
SummitSummer <benzofurialesss@gmail.com>
2025-12-29 02:58:39 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,13 @@ import Cart from "@/pages/Cart";
 import Checkout from "@/pages/Checkout";
 import Auth from "@/pages/Auth";
 import NotFound from "@/pages/not-found";
+// Footer pages
+import ContactUs from "@/pages/ContactUs";
+import FAQs from "@/pages/FAQs";
+import ShippingReturns from "@/pages/ShippingReturns";
+import WarrantyPolicy from "@/pages/WarrantyPolicy";
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
+import TermsOfService from "@/pages/TermsOfService";
 
 function Router() {
   return (
@@ -26,6 +33,13 @@ function Router() {
           <Route path="/cart" component={Cart} />
           <Route path="/checkout" component={Checkout} />
           <Route path="/auth" component={Auth} />
+          {/* Footer pages */}
+      <Route path="/contact-us" component={ContactUs} />
+      <Route path="/faqs" component={FAQs} />
+      <Route path="/shipping-returns" component={ShippingReturns} />
+      <Route path="/warranty-policy" component={WarrantyPolicy} />
+      <Route path="/privacy-policy" component={PrivacyPolicy} />
+      <Route path="/terms-of-service" component={TermsOfService} />
           <Route component={NotFound} />
         </Switch>
       </main>
