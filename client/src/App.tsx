20dbfee3846b5b021ad4a59a2738f3b26e2c70ed
SummitSummer <benzofurialesss@gmail.com>
2025-12-29 18:34:27 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navbar } from "@/components/Navbar";
-import { SupportLayout } from "@/components/SupportLayout";
+import SupportLayout from "@/components/SupportLayout";
 
 // Pages
 import Home from "@/pages/Home";
